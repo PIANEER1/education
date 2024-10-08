19fd1a3d4220859320ee9a0fe13008fa62dcebc7
PIANEER1 <le6a.novikov@gmail.com>
2024-10-08 19:41:38 +0300

    Add files via upload

---
+++ Email.py
@@ -0,0 +1,28 @@
+"""
+Задача 3. Email-адреса
+
+Данные об email-адресах студентов хранятся в словаре:
+emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
+      	'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+      	'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+      	'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+      	'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+      	'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+
+Нужно дополнить код таким образом, чтобы он вывел все адреса в алфавитном порядке и в 
+формате имя_пользователя@домен.
+"""
+
+emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
+      	'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+      	'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+      	'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+      	'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+      	'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+индусамскажимнет = []
+
+for key in emails:
+    for values in emails[key]:
+        индусамскажимнет.append(f'{values}@{key}')
+
+print(*индусамскажимнет)
---
+++ count namber.py
@@ -0,0 +1,21 @@
+"""
+ввод: 6 5 4 7 6 8 6
+вывод:
+d = {
+6:3
+5:1
+4:1
+7:1
+8:1
+}
+
+"""
+
+a = input().split()
+q = []
+d = {}
+for i in a:
+        i = a.count(i)
+        q.append(i)
+print(d)
+
